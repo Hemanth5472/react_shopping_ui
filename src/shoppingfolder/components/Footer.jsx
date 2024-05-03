f580fde22faace4ca6250dd8749f21207a00b86f
Hemanth5472git config --global user.name Hemanth5472git config --global user.name Hemanthgit cinfig --global user.name Hemanth-5472 <ahemanthbabu2001@gmail.com>
2024-05-03 22:14:41 +0530

    react_shopping_ui

---
+++ src/shoppingfolder/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footerSection'>
+            <div className='bannerBox'>
+      <img src="assest/footer.png" alt="banner" />
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
